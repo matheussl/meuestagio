8bda33f32a64b4466e7472fd8dc69f628f18b190
Matheus Lima <matheus.se@gmail.com>
2014-03-21 16:08:19 -0300

    remove o botão de logar quando logado e adiciona o logout

--- meuestagio/apps/public/urls.py
+++ meuestagio/apps/public/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-    url(r'^(?P<slug>[^/.]+)/$', views.area, name='area'),
-    url(r'^(?P<slug_area>[^\.]+)/(?P<slug>[^\.]+)/$', views.estagio, name='estagio'),
+    url(r'^vagas/(?P<slug>[^/.]+)/$', views.area, name='area'),
+    url(r'^vagas/(?P<slug_area>[^\.]+)/(?P<slug>[^\.]+)/$', views.estagio, name='estagio'),
 )
--- meuestagio/meuestagio/urls.py
+++ meuestagio/meuestagio/urls.py
@@ -5,6 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'', include('apps.public.urls')),
+
 )
